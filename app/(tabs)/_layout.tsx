4b2caf9b0d66aa29264fe96b41c8f7080b3c7263
Green Gem <chris_waitt@me.com>
2024-03-27 10:40:56 +0000

    Creaet tab menu for navigation, create modal popup for new categories

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,44 +1,39 @@
-import React from 'react';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link, Tabs } from 'expo-router';
-import { Pressable } from 'react-native';
-
-import Colors from '@/constants/Colors';
-import { useColorScheme } from '@/components/useColorScheme';
-import { useClientOnlyValue } from '@/components/useClientOnlyValue';
-
-// You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
-function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
-  color: string;
-}) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
-}
+import { Link, Tabs } from 'expo-router'
+import { Pressable } from 'react-native'
+import { IconDashboard, IconCreditCardPay, IconInfoCircle, IconHistory, IconCategory } from '@tabler/icons-react-native';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        // Disable the static render of the header on web
-        // to prevent a hydration error in React Navigation v6.
-        headerShown: useClientOnlyValue(false, true),
-      }}>
+        tabBarActiveTintColor: 'red',
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          title: 'Dashboard',
+          tabBarIcon: ({ color }) => <IconDashboard size={28} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="new-transaction"
+        options={{
+          title: 'New Transaction',
+          tabBarIcon: ({ color }) => <IconCreditCardPay color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="categories"
+        options={{
+          title: 'Categories',
+          tabBarIcon: ({ color }) => <IconCategory color={color} />,
           headerRight: () => (
-            <Link href="/modal" asChild>
+            <Link href="/new-category-modal" asChild>
               <Pressable>
                 {({ pressed }) => (
-                  <FontAwesome
-                    name="info-circle"
+                  <IconInfoCircle
                     size={25}
-                    color={Colors[colorScheme ?? 'light'].text}
                     style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
                   />
                 )}
@@ -48,12 +43,12 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="two"
+        name="history"
         options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          title: 'History',
+          tabBarIcon: ({ color }) => <IconHistory color={color} />,
         }}
       />
     </Tabs>
-  );
+  )
 }
