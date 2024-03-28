eab8ba4e493fb54e5959d219c2892a43e718e4f5
Green Gem <chris_waitt@me.com>
2024-03-28 15:41:10 +0000

    Refactor TabOneScreen component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,46 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import { View, Text } from 'react-native';
+import { useFocusEffect } from 'expo-router';
+import { useState, useCallback } from 'react';
+import { fetchCurrentBalance, deleteAllData } from '../services/database';
+import Card from '@/components/Card';
+import { Button } from '@/components/Button';
 
 export default function TabOneScreen() {
+  const [currentBalance, setCurrentBalance] = useState<number | null>(null);
+
+  useFocusEffect(
+    useCallback(() => {
+      const loadCurrentBalance = async () => {
+        try {
+          const balance = await fetchCurrentBalance();
+          setCurrentBalance(balance);
+        } catch (error) {
+          console.error('Failed to fetch current balance:', error);
+        }
+      };
+
+      loadCurrentBalance();
+    }, [])
+  );
+
+  // DEBUG: Purge all data and recreate categories
+  const handleDeleteAllData = async () => {
+    try {
+      await deleteAllData();
+      setCurrentBalance(null);
+    } catch (error) {
+      console.error('Failed to delete all data:', error);
+    }
+  };
+
+  const formattedBalance = currentBalance !== null ? `£${currentBalance.toFixed(2)}` : '$0.00';
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+    <View className="p-5 ctp-mocha">
+      <Card title='Expenses'>
+        <Text className='text-4xl font-semibold text-ctp-text'>{formattedBalance}</Text>
+      </Card>
+      {/* <Button variant='danger' onPress={handleDeleteAllData} title='Delete all data' /> */}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
