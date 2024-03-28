ff567e48267223ead88d929090db006036db4148
Green Gem <chris_waitt@me.com>
2024-03-28 15:41:22 +0000

    Add Card component

---
+++ components/Card.tsx
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native';
+
+interface CardProps {
+  children: React.ReactNode;
+  title?: string;
+}
+
+export default function Card({ children, title }: CardProps) {
+  return (
+    <View className='bg-ctp-surface0 rounded-2xl px-4 py-3 mb-3 flex flex-col gap-3'>
+      {title && <Text className='text-ctp-text'>{title}</Text>}
+      {children}
+    </View>
+  );
+}
