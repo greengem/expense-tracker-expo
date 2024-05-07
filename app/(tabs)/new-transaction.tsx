f2348102b7e046e10e7ea0a263ffec49dbc69770
Green Gem <chris_waitt@me.com>
2024-05-07 17:48:14 +0100

    feat: Add category color selection to new category modal screen

--- app/(tabs)/new-transaction.tsx
+++ app/(tabs)/new-transaction.tsx
@@ -1,8 +1,7 @@
-import { useState,  useCallback } from 'react';
+import { useState,  useCallback, useEffect } from 'react';
 import { useFocusEffect } from 'expo-router';
-import { useForm, Controller } from 'react-hook-form';
 import DateTimePicker from '@react-native-community/datetimepicker';
-import { View, Text, Pressable } from 'react-native';
+import { View, ScrollView, Text, Pressable, Modal, FlatList, TouchableOpacity } from 'react-native';
 import { fetchCategories, addTransaction } from '../services/database';
 import { router } from 'expo-router';
 import Input from '@/components/Input';
@@ -18,22 +17,20 @@ interface FormData {
 interface Category {
   id: number;
   name: string;
+  color: string;
 }
 
+
+
 export default function TabNewTransactionScreen() {
+  // States
   const [date, setDate] = useState(new Date());
   const [categories, setCategories] = useState<Category[]>([]);
-  const [showCatPicker, setShowCatPicker] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState<Category | null>(null);
+  const [formErrors, setFormErrors] = useState<{ [K in keyof FormData]?: string }>({});
+  const [isModalVisible, setModalVisible] = useState(false);
 
-  const { control, handleSubmit, formState: { errors }, reset, trigger, setError } = useForm<FormData>({
-    defaultValues: {
-      amount: '',
-      category: null,
-      note: '',
-    }
-  });
-  
-
+  // Load Categories on Focus
   useFocusEffect(
     useCallback(() => {
       const loadCategories = async () => {
@@ -44,21 +41,34 @@ export default function TabNewTransactionScreen() {
     }, [])
   );
 
-  const onSubmit = async (data: FormData) => {
-    console.log('Form data:', data);
-    const isFormValid = await trigger();
-    if (data.category === null) {
-      setError('category', {
-        type: 'manual',
-        message: 'You must select a category',
-      });
-      return;
+  const [formData, setFormData] = useState<FormData>({
+    amount: '',
+    category: null,
+    note: '',
+  });
+
+  const onSubmit = async () => {
+    console.log('Form data:', formData);
+    const newFormErrors: { [K in keyof FormData]?: string } = {};
+
+    if (!formData.amount) {
+      newFormErrors.amount = 'You must enter an amount';
     }
-  
-    if (isFormValid) {
+
+    if (formData.category === null) {
+      newFormErrors.category = 'You must select a category';
+    }
+
+    setFormErrors(newFormErrors);
+
+    if (Object.keys(newFormErrors).length === 0 && formData.category !== null) {
       try {
-        await addTransaction(Number(data.amount), data.category, date.toISOString(), data.note || '');
-        reset();
+        await addTransaction(Number(formData.amount), formData.category, date.toISOString(), formData.note || '');
+        setFormData({
+          amount: '',
+          category: null,
+          note: '',
+        });
         setDate(new Date());
         router.push('/');
       } catch (error) {
@@ -67,85 +77,81 @@ export default function TabNewTransactionScreen() {
     }
   };
 
-  const togglePicker = () => {
-    console.log('Toggling picker');
-    setShowCatPicker(prevState => !prevState);
+  const onCategoryChange = (selectedCat: Category | null) => {
+    console.log('Selected category:', selectedCat);
+    setSelectedCategory(selectedCat);
+    setModalVisible(false);
+    if (selectedCat) {
+      setFormData({ ...formData, category: selectedCat.id });
+    }
   };
-  
 
   return (
     <View className='p-3 ctp-latte dark:ctp-mocha flex flex-col gap-3'>
-
-      <Controller
-        control={control}
-        render={({ field: { onChange, onBlur, value } }) => (
-          <Input 
-            placeholder='£0.00'
-            value={value}
-            onBlur={onBlur}
-            onChangeText={onChange}
-            keyboardType="numeric"
-          />
-        )}
-        name="amount"
-        rules={{ required: 'You must enter an amount' }}
+      <Input 
+        placeholder='£0.00'
+        value={formData.amount}
+        onChangeText={(text) => setFormData({ ...formData, amount: text })}
+        keyboardType="numeric"
       />
-      {errors.amount && <Text className='text-ctp-red'>{errors.amount.message}</Text>}
+      {formErrors.amount && <Text className='text-ctp-red'>{formErrors.amount}</Text>}
 
-      <Controller
-        control={control}
-        render={({ field: { onChange, onBlur, value } }) => (
+      <Pressable onPress={() => setModalVisible(true)}>
+        <View pointerEvents="none">
           <Input
-            onBlur={onBlur}
-            onChangeText={onChange}
-            value={value}
-            placeholder="Note"
+            editable={false}
+            value={selectedCategory ? selectedCategory.name : ''}
+            placeholder="Select a category"
           />
-        )}
-        name="note"
-      />
-      <Pressable onPress={togglePicker}>
-        <Input 
-          editable={false}
-        />
+        </View>
       </Pressable>
 
-      {showCatPicker && (
-      <Controller
-        control={control}
-        render={({ field: { onChange, onBlur, value } }) => (
-          <Picker
-            selectedValue={value}
-            onValueChange={(itemValue, itemIndex) => {
-              onChange(itemValue);
-              setShowCatPicker(false);
-            }}
-            onBlur={onBlur}
-          >
-            {categories.map((category) => (
-              <Picker.Item color='white' label={category.name} value={category.id.toString()} key={category.id.toString()} />
-            ))}
-          </Picker>
-        )}
-        name="category"
-      />
-      )}
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={isModalVisible}
+        onRequestClose={() => {
+          setModalVisible(!isModalVisible);
+        }}
+      >
+        <View className="p-5 ctp-latte dark:ctp-mocha bg-ctp-crust flex grow py-20">
+            <FlatList
+              data={categories}
+              keyExtractor={(item) => item.id.toString()}
+              renderItem={({ item }) => (
+                <TouchableOpacity onPress={() => onCategoryChange(item)}>
+                <View key={item.id} className='flex flex-row items-center py-3 gap-3'>
+                  <View className='h-5 w-5 rounded-full' style={{backgroundColor: item.color}} />
+                  <Text className='text-ctp-text'>{item.name}</Text>
+                </View>
+              </TouchableOpacity>
+              )}
+            />
+        </View>
+      </Modal>
 
+      {/* 
       <View className='flex flex-row justify-center p-0 -ml-3'>
         <DateTimePicker
           className='w-full'
           value={date}
           mode="date"
-          display="default"
+          display="spinner"
           onChange={(event, selectedDate) => {
             const currentDate = selectedDate || date;
             setDate(currentDate);
           }}
         />
-      </View>
+      </View> 
+      */}
 
+      <Input
+        onChangeText={(text) => setFormData({ ...formData, note: text })}
+        value={formData.note}
+        placeholder="Note"
+      />
 
-      <Button onPress={handleSubmit(onSubmit)} title='Add Transaction' />
+      <Button onPress={onSubmit} title='Add Transaction' />
     </View>
   );
 }
