a0b8612a85a9bd4a76161dc494861e2029526145
Green Gem <chris_waitt@me.com>
2024-05-07 17:46:31 +0100

    Add labelColors module with predefined color values

---
+++ components/labelColors.ts
@@ -0,0 +1,15 @@
+export const labelColors = [
+    '#f2cdcd', 
+    '#f5c2e7', 
+    '#cba6f7', 
+    '#f38ba8', 
+    '#eba0ac', 
+    '#fab387', 
+    '#f9e2af',
+    '#a6e3a1',
+    '#94e2d5',
+    '#89dceb',
+    '#74c7ec',
+    '#89b4fa',
+    '#b4befe',
+];
