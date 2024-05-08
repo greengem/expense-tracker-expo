5a1f48d20a1b9aab768d17b0360dbe7f30739658
Green Gem <chris_waitt@me.com>
2024-05-08 10:38:47 +0100

    refactor: Update Card component layout to remove vertical gap

--- components/Card.tsx
+++ components/Card.tsx
@@ -7,7 +7,7 @@ interface CardProps {
 
 export default function Card({ children, title }: CardProps) {
   return (
-    <View className='bg-ctp-surface0 rounded-2xl px-4 py-3 mb-3 flex flex-col gap-3'>
+    <View className='bg-ctp-surface0 rounded-2xl px-4 py-3 mb-3 flex flex-col gap-0'>
       {title && <Text className='text-ctp-text'>{title}</Text>}
       {children}
     </View>
